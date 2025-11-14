4c79a5c89b5761abff19bd92f19385bb86345f27
Gautam Yadav <bitengineer09@gmail.com>
2025-11-14 21:01:05 +0530

    updated index.js

--- Backend/index.js
+++ Backend/index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT;
 
 app.use(express.urlencoded({extended: true}));
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://convo-frontend-shfw.onrender.com",
     credentials: true,
 }));
 app.use(express.json());
